034406ea30a5f01817ca56a5eda80c373b05793f
mika091102 <mikaeddys@gmail.com>
2026-08-25 14:01:13 +0300

    creatin des api call dans front pour register , login, createResto et uploadPhotoResto

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -17,3 +17,52 @@ const res = await fetch(`${API_URL}/restaurants/${id}`);
 if (!res.ok) throw new Error (" error whwne loading restaurant");
 return res.json();
 }
+
+export async function registerUser (nom , email, mot_de_passe){
+  const res = await fetch(`${API_URL}/auth/register`,{
+    method: "POST",
+    headers : {"Content-type":"application/json"},
+    body:JSON.stingify({nom, email, mot_de_passe}),
+  });
+
+  const data = await res.json();
+  if (!res.ok) throw new Error (data.message || "Register error");
+  return data;
+}
+export async function loginUser (email, mot_de_passe){
+  const res = await fetch (`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {"Content-type": "application/json"},
+    body: JSON.stingify({email, mot_de_passe}), 
+  });
+  const data = await res.json();
+  if (!res.ok) throw new Error (data.massege || "Login error");
+  return data;
+}
+
+//appeler api pour ....
+export async function createResto(payload, token) {
+const res = await fetch (`${API_URL}/restaurant`, {
+  method:"POST",
+  headers: {
+    "Content-type":"application/json", 
+  Authorization: `Bearer ${token}`},
+  body:JSON.stringify(payload),
+
+});
+const data = await res.json();
+if(!res.ok) throw new Error (data.message || "Error creating Resto");
+return data;
+}
+
+export async function uploadPhotoResto (restaurantID, file, token) {
+  const res = await fetch (`${API_URL}/restaurant/${restaurantID}/photo`, {
+    methode: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`},
+      body:FormData,
+  });
+  const data = await res.json();
+  if(!res.ok) throw new Error (data.message || "error uploading photo");
+  return data;
+}
